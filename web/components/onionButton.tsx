d671b00005e0bea21f7b021b155415080bea8446
Kakudo Kentaro <31888089+kakudo415@users.noreply.github.com>
2023-07-01 22:40:16 +0900

    feat: Make styles more better

--- web/components/onionButton.tsx
+++ web/components/onionButton.tsx
@@ -5,19 +5,24 @@ import { center } from '@/styled-system/patterns'
 
 function Screw() {
   return (
-    <div className={css({height: '32px', width: '32px', rounded: '50%', background: '#90A4AE' })}></div>
+    <div className={css({ height: '32px', width: '32px', rounded: '50%', background: '#90A4AE', boxShadow: '0px 3px 0 0 #78909C' })}></div>
   )
+}
+
+function onionClick() {
+  // TODO: Push Notification
+  console.log('ONION CLICKED!')
 }
 
 export default function OnionButton() {
   return (
-    <div className={center({ aspectRatio: '1/1', width: '100%', rounded: '32px', background: '#CFD8DC', boxShadow: '0px 8px 0 0 #90A4AE', position: 'relative' })}>
+    <div className={center({ aspectRatio: '1/1', width: '100%', rounded: '32px', background: '#CFD8DC', boxShadow: '0px 16px 0 0 #90A4AE', position: 'relative' })}>
       <div className={`${css({ position: 'absolute', top: '32px', left: '32px' })}`}><Screw /></div>
       <div className={`${css({ position: 'absolute', top: '32px', right: '32px' })}`}><Screw /></div>
       <div className={`${css({ position: 'absolute', bottom: '32px', left: '32px' })}`}><Screw /></div>
       <div className={`${css({ position: 'absolute', bottom: '32px', right: '32px' })}`}><Screw /></div>
 
-      <button className={center({ aspectRatio: '1/1', width: '50%', rounded: '50%', background: '#F57C00', position: 'relative', top: '-12px', boxShadow: '0px 12px 0 0 #E65100', _active: { top: '-2px', boxShadow: '0px 2px 0 0 #E65100' } })}>
+      <button onClick={onionClick} className={center({ aspectRatio: '1/1', width: '50%', rounded: '50%', background: '#F57C00', position: 'relative', top: '-16px', boxShadow: '0px 16px 0 0 #E65100', cursor: 'pointer', _hover: { background: '#EF6C00' }, _active: { top: '-0px', boxShadow: 'none' } })}>
         <img src='/icon-512x512.png' alt='Tamanegi' className={css({ height: 'auto', width: '60%' })} />
       </button>
     </div>
